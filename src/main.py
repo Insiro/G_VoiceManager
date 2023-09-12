63d1475c3031f6b0d0659d9ac0fed85c0804f812
Insiro <leeon@insiro.me>
2023-09-12 16:13:10 +0900

    refactor: change structure

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from repack import repack
+
+
+wav_path = "../temp/repack/wem/"
+input_path = "../temp/input_pck/"
+output_path = "../temp/output_pck/"
+
+
+repack.repact(wav_path, input_path, output_path)
--- src/repack/repack.py
+++ src/repack/repack.py
@@ -1,25 +1,29 @@
 import os
-from FilePackager import Package, build_pck_file
+from .FilePackager import Package, build_pck_file
 
-wav_path = './wem/'
-input_path = './input_pck/'
-output_path = './output_pck/'
 
-for i in os.scandir(wav_path):
-    if os.path.exists(output_path+i.name+'.pck') == False:
-        print(output_path+i.name+'.pck')
-        modify_pck = Package()  # init
-        modify_pck.addfile(open(input_path+i.name+'.pck', 'rb'))  # add the pck file you want to modify
-        for f in os.scandir(wav_path+i.name):
-            int_hash_value = int(f.name.replace('.wem',''), 16) # In the unpacked file, its hash is in the file name
-            modify_pck.add_wem(2, 0, int_hash_value, open(wav_path+i.name+'/'+f.name, 'rb'))  # override the wem file
-            # Where 1 argument - external files
-            # 0: soundbanks
-            # 1: stream files
-            # 2: external files
+def repact(wav_path: str, input_path: str, output_path: str):
+    for i in os.scandir(wav_path):
+        if os.path.exists(output_path + i.name + ".pck") == False:
+            print(output_path + i.name + ".pck")
+            modify_pck = Package()  # init
+            modify_pck.addfile(
+                open(input_path + i.name + ".pck", "rb")
+            )  # add the pck file you want to modify
+            for f in os.scandir(wav_path + i.name):
+                int_hash_value = int(
+                    f.name.replace(".wem", ""), 16
+                )  # In the unpacked file, its hash is in the file name
+                modify_pck.add_wem(
+                    2, 0, int_hash_value, open(wav_path + i.name + "/" + f.name, "rb")
+                )  # override the wem file
+                # Where 1 argument - external files
+                # 0: soundbanks
+                # 1: stream files
+                # 2: external files
 
-            # Where 2 argument - language id
-            # You can not change it, since the genshin almost always has SFX
+                # Where 2 argument - language id
+                # You can not change it, since the genshin almost always has SFX
 
-        with open(output_path+i.name+'.pck', 'wb') as f:
-            build_pck_file(modify_pck, f, modify_pck.LANGUAGE_DEF)
+            with open(output_path + i.name + ".pck", "wb") as f:
+                build_pck_file(modify_pck, f, modify_pck.LANGUAGE_DEF)
