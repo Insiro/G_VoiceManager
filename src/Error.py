a2ab683fefa8a5f091a5fd9ce42cff142e5310d5
Insiro <leeon@insiro.me>
2023-09-12 20:58:49 +0900

    add: Error Types

---
+++ src/Error.py
@@ -0,0 +1,24 @@
+class ModManagerException(Exception):
+    _msg = "DirNotReady"
+
+    def __init__(self, msg: str | None = None) -> None:
+        if msg is not None:
+            self._msg = msg
+
+    def get_msg(self) -> str:
+        return self._msg
+
+
+class ModNameNotValidException(ModManagerException):
+    _msg = "Not Valid Mod Name"
+
+
+class ModSourceNotReadyException(ModManagerException):
+    _msg = "not prepared mod source files"
+
+
+class NoGeneratedModFiles(ModManagerException):
+    _msg = "generated mod files not Exists"
+
+class NotValidPathException(ModManagerException):
+    _msg = "Not Valid Path"
